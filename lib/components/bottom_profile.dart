a29f5905a8cf1a5ec2f8328d7c310867671a6155
Higor Pires de França <higorpiresdefranca60@gmail.com>
2022-10-12 22:29:34 -0300

    chore: add BottomProfile

---
+++ lib/components/bottom_profile.dart
@@ -0,0 +1,51 @@
+import 'package:app_flutter/components/button_option_profile.dart';
+import 'package:flutter/material.dart';
+
+class BottomProfile extends StatelessWidget {
+  const BottomProfile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Ink(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              spreadRadius: 1,
+              blurRadius: 10,
+              offset: const Offset(0, 3), // changes position of shadow
+            ),
+          ],
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ButtonOptionProfile(
+              label: "Lista de Interesse",
+              icon: Icons.book_rounded,
+              topLeftRadius: 10,
+              topRightRadius: 10,
+              onPressed: () {},
+            ),
+            ButtonOptionProfile(
+              label: "Livros Disponibilizados",
+              icon: Icons.book_rounded,
+              onPressed: () {},
+            ),
+            ButtonOptionProfile(
+              label: "Livros Avaliados",
+              icon: Icons.stars_rounded,
+              bottomLeftRadius: 10,
+              bottomRightRadius: 10,
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
