e976a3b63ca83544b1c44e496db57108d614c466
Higor Pires de França <higorpiresdefranca60@gmail.com>
2022-10-26 12:09:11 -0300

    add: pages radius const

--- lib/configs/themes.dart
+++ lib/configs/themes.dart
@@ -6,6 +6,8 @@ const dark = Color(0xFF252837);
 const grey = Color(0xFF646E77);
 const lightGrey = Color(0xFFF6F7F8);
 
+const double pageRadius = 30;
+
 ThemeData themeData = ThemeData(
   primarySwatch: Colors.red,
   fontFamily: "Avenir",
