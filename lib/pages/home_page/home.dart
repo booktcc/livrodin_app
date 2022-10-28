943402aaa14a383a731a4001d87be1ed9d7ff809
Higor Pires de França <higorpiresdefranca60@gmail.com>
2022-10-28 14:58:21 -0300

    chore: add page titles

--- lib/pages/home_page/home.dart
+++ lib/pages/home_page/home.dart
@@ -1,4 +1,7 @@
+import 'package:app_flutter/components/home_banner.dart';
+import 'package:app_flutter/components/title.dart';
 import 'package:app_flutter/configs/themes.dart';
+import 'package:app_flutter/models/book.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -6,24 +9,57 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const SizedBox(
-          height: 240,
-        ),
-        Flexible(
-          child: Ink(
-            width: double.infinity,
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(30),
-                topRight: Radius.circular(30),
+    return LayoutBuilder(
+      builder: (context, boxConstraints) {
+        final double minSize =
+            (boxConstraints.maxHeight - 240) / boxConstraints.maxHeight;
+        return Stack(
+          children: [
+            // HomeBanner(
+            //   books: const [],
+            // ),
+            DraggableScrollableSheet(
+              initialChildSize: minSize,
+              maxChildSize: 1,
+              minChildSize: minSize,
+              builder: (context, scrollController) => ClipRRect(
+                borderRadius: const BorderRadius.vertical(
+                  top: Radius.circular(pageRadius),
+                ),
+                child: Container(
+                  color: lightGrey,
+                  child: CustomScrollView(
+                    controller: scrollController,
+                    slivers: [
+                      SliverToBoxAdapter(
+                        child: PageTitle(
+                          title: "Últimos Disponibilizados",
+                          actions: [
+                            IconButton(
+                              icon: const Icon(Icons.more_rounded),
+                              onPressed: () {},
+                            ),
+                          ],
+                        ),
+                      ),
+                      SliverToBoxAdapter(
+                        child: SizedBox(
+                          height: 200,
+                        ),
+                      ),
+                      SliverToBoxAdapter(
+                        child: PageTitle(
+                          title: "Recomendados",
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              color: lightGrey,
             ),
-          ),
-        ),
-      ],
+          ],
+        );
+      },
     );
   }
 }
