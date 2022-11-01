21be23d3239c2fd75380f3e11f493c41e65cf24b
Higor Pires de França <higorpiresdefranca60@gmail.com>
2022-10-31 23:11:59 -0300

    fix: splash color

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -27,49 +27,50 @@ class ProfilePage extends StatelessWidget {
           ),
         ],
       ),
-      child: Container(
-        decoration: const BoxDecoration(
-          color: lightGrey,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(pageRadius),
-            topRight: Radius.circular(pageRadius),
-          ),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(pageRadius),
+          topRight: Radius.circular(pageRadius),
         ),
-        height: double.infinity,
-        width: double.infinity,
-        child: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(right: 20, left: 20, top: 24),
-                child: ProfileInfo(
-                  name: authController.user.value?.displayName ?? 'Sem nome',
-                  email: authController.user.value?.email ?? 'Sem email',
-                  image: "https://avatars.githubusercontent.com/u/47704204?v=4",
+        child: Container(
+          color: lightGrey,
+          height: double.infinity,
+          width: double.infinity,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 20, left: 20, top: 24),
+                  child: ProfileInfo(
+                    name: authController.user.value?.displayName ?? 'Sem nome',
+                    email: authController.user.value?.email ?? 'Sem email',
+                    image:
+                        "https://avatars.githubusercontent.com/u/47704204?v=4",
+                  ),
                 ),
-              ),
-              const SizedBox(height: 24),
-              const TransactionPainel(
-                icon: Icons.handshake_rounded,
-                title: 'Doações',
-              ),
-              const SizedBox(height: 10),
-              const TransactionPainel(
-                icon: Icons.swap_horizontal_circle_rounded,
-                title: 'Trocas',
-              ),
-              const SizedBox(height: 10),
-              const BottomProfile(),
-              const SizedBox(height: 10),
-              ButtonAction(
-                minWidth: 100,
-                icon: Icons.logout,
-                label: 'Sair',
-                onPressed: authController.logout,
-              ),
-              const SizedBox(height: 10),
-            ],
+                const SizedBox(height: 24),
+                const TransactionPainel(
+                  icon: Icons.handshake_rounded,
+                  title: 'Doações',
+                ),
+                const SizedBox(height: 10),
+                const TransactionPainel(
+                  icon: Icons.swap_horizontal_circle_rounded,
+                  title: 'Trocas',
+                ),
+                const SizedBox(height: 10),
+                const BottomProfile(),
+                const SizedBox(height: 10),
+                ButtonAction(
+                  minWidth: 100,
+                  icon: Icons.logout,
+                  label: 'Sair',
+                  onPressed: authController.logout,
+                ),
+                const SizedBox(height: 10),
+              ],
+            ),
           ),
         ),
       ),
