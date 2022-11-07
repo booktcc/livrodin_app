f804ac98c51ecae57f9f21673594807b85e72ac0
João Lucas de Oliveira Lopes <55464917+jlucaso1@users.noreply.github.com>
2022-11-07 17:21:24 -0300

    fix: ci/cd and add a rule in firestore (#42)

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:livrodin/configs/themes.dart';
@@ -32,32 +29,12 @@ import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  if (kDebugMode) {
-    await dotenv.load(fileName: '.env');
-  }
+
   initializeDateFormatting('pt_BR', null);
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  if (dotenv.env['USE_FIREBASE_EMULATOR'] == 'true') {
-    var host = dotenv.env['FIREBASE_EMULATOR_HOST'];
-    if (host != null) {
-      debugPrint('Using Firebase Emulator at $host');
-      FirebaseFirestore.instance.useFirestoreEmulator(host, 8080);
-      FirebaseFirestore.instance.settings = const Settings(
-        sslEnabled: false,
-        persistenceEnabled: false,
-      );
-
-      FirebaseAuth.instance.useAuthEmulator(host, 9099);
-
-      FirebaseFunctions.instanceFor(region: 'southamerica-east1')
-          .useFunctionsEmulator(host, 5001);
-
-      FirebaseStorage.instance.useStorageEmulator(host, 9199);
-    }
-  }
 
   runApp(const MyApp());
 }
